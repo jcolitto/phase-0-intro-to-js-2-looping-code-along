0d506fb4ce94d948d1a8c1ab27eb4e494da5d88e
Joseph <jcolitto@mac.com>
2023-05-20 22:03:24 -0500

    Updated

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(names, event) {
+    for(let i = 0; i < names.length; i++ ) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`; 
+        messages.push(message);       
+    }
+
+    return messages;
+}
+
+// console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown() {
+let count = 10;
+while (count > 0 - 1) {
+    console.log(count--);
+}
+}
+console.log(countDown(10));
+
+
